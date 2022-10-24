0c7507386b3016253df18f1b2a8a758705b29b31
gustavonasce <115467323+gustavonasce@users.noreply.github.com>
2022-10-24 09:08:38 -0300

    Add files via upload

---
+++ bdloja.sql
@@ -0,0 +1,42 @@
+create table livro(
+id_livro int not null auto_increment,
+nome varchar(50),
+palavra_chave varchar(50),
+descricao varchar(500),
+primary key(id_livro)
+)default charset=utf8;
+
+create table editora(
+id_editora int not null auto_increment,
+nome varchar(50),
+telefone varchar(50),
+endereco varchar(500),
+primary key(id_editora)
+)default charset=utf8;
+
+create table autor(
+id_autor int not null auto_increment,
+nome varchar(50),
+telefone varchar(50),
+endereco varchar(50),
+primary key(id_autor)
+)default charset=utf8;
+
+create table usuario(
+id_usuario int not null auto_increment,
+nome varchar(50),
+telefone varchar(50),
+endereco varchar(50),
+primary key(id_usuario)
+)default charset=utf8;
+
+create table emprestimo(
+id_emprestimo int not null auto_increment,
+data_emprestimo date,
+data_devolucao date,
+primary key(id_emprestimo)
+)default charset=utf8;
+
+
+
+
